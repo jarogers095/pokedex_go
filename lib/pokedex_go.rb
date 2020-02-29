e5299dd057f560b151d9d3c1f58c64b7ead697ff
James Rogers <jarogers095@gmail.com>
2020-02-29 14:36:55 -0800

    Completed initial version of /lib/pokedex_go.rb, listing require statements for .rb files in /lib/pokedex_go

--- lib/pokedex_go.rb
+++ lib/pokedex_go.rb
@@ -2,5 +2,8 @@ require "pokedex_go/version"
 
 module PokedexGo
   class Error < StandardError; end
-  # Your code goes here...
+  require "pokedex_go/ascii_image_converter"
+  require "pokedex_go/cli"
+  require "pokedex_go/pokemon"
+  require "pokedex_go/scraper"
 end
