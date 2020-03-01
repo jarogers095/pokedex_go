0582a1d4169a1f63bc0220446caaf4935645ba0b
James Rogers <jarogers095@gmail.com>
2020-02-29 22:20:44 -0800

    Added gem requirements to /lib/pokedex_go: pry, nokogiri, open-uri

--- lib/pokedex_go.rb
+++ lib/pokedex_go.rb
@@ -2,6 +2,11 @@ require "pokedex_go/version"
 
 module PokedexGo
   class Error < StandardError; end
+  
+  require "pry"
+  require "nokogiri"
+  require "open-uri"
+
   require "pokedex_go/ascii_image_converter"
   require "pokedex_go/cli"
   require "pokedex_go/pokemon"
