2751c13591116ff0de6ab9afd96883263d517856
James Rogers <jarogers095@gmail.com>
2020-03-01 00:03:30 -0800

    Added methods to Scraper class for scraping individual pokemon profiles and adding the data to existing pokemon objects

--- lib/pokedex_go/scraper.rb
+++ lib/pokedex_go/scraper.rb
@@ -7,7 +7,11 @@ class PokedexGo::Scraper
     def self.scrape_pokemon_index()
         return get_page("https://gamepress.gg/pokemongo/pokemon-list").css(".pokemon-row")
     end
-    
+
+    def self.scrape_pokemon_profile(profile_url)
+        return get_page(profile_url)
+    end
+        
     def self.create_pokemon_from_index()
         scrape_pokemon_index().each do |entry|
             attributes = {
@@ -22,11 +26,27 @@ class PokedexGo::Scraper
                 attack: entry.attribute("data-atk").value,
                 defense: entry.attribute("data-def").value,
                 max_cp: entry.attribute("data-cp").value,
-                overall_rating: entry.attribute("data-rating").value,
+                pve_rating: entry.attribute("data-rating").value,
                 type: entry.attribute("data-type").value,
                 profile_url: entry.css("td a").first.attribute("href").value,
             }
             Pokemon.new(attributes)
         end
+    end
+
+    def self.add_profile_stats(pokemon)
+        profile_page = scrape_pokemon_profile(pokemon.profile_url)
+        profile_stats = {
+            weight: profile_page.css(".pokemon-weight").text.strip(),
+            height: profile_page.css(".pokemon-height").text.strip(),
+            fast_moves: profile_page.css(".field--name-field-primary-moves .pokemon-page-moves-item"),
+            charge_moves: profile_page.css(".field--name-field-secondary-moves .pokemon-page-moves-item"),
+            female_ratio: profile_page.css(".female-percentage").text.strip(),
+            weaknesses: profile_page.css("#weak-table tbody tr"),
+            resistances: profile_page.css("#resist-table tbody tr")
+        }
+
+        pokemon.add_stats(profile_stats)
+        return pokemon
     end
 end
