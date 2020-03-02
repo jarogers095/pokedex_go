94d0a018cd6ab2acd12da184d5cc150546629207
James Rogers <jarogers095@gmail.com>
2020-03-01 16:12:31 -0800

    Created CLI class and its stubs

---
+++ lib/pokedex_go/cli.rb
@@ -0,0 +1,25 @@
+class CLI
+    def call()
+        #entrypoint
+    end
+
+    def welcome()
+        #initial welcome message
+    end
+
+    def main_menu()
+        #Root of menu system
+    end
+
+    def list_all_pokemon()
+        #presents entire list of pokemon, broken up into segments
+    end
+
+    def list_pokemon_by_type(type)
+        #lists all pokemon of given type
+    end
+
+    def view_pokemon_profile(pokemon)
+        #view full profile of individual pokemon
+    end
+end
