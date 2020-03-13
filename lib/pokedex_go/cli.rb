b5403b7f54de98783e66cba54df2180854a80633
James Rogers <jarogers095@gmail.com>
2020-03-13 13:14:07 -0700

    Added comments to CLI

--- lib/pokedex_go/cli.rb
+++ lib/pokedex_go/cli.rb
@@ -17,20 +17,23 @@ class PokedexGo::CLI
         reverse: "\u001b[7m"
     }
 
+    #entrypoint - called from executable bin/pokedex_go
     def self.call()
-        #entrypoint
+        #calls welcome to initiate setup
         welcome()
     end
 
+    #Welcome adjusts terminal window size if allowed, and 
+    ##initiates the scraping of the Pokemon site to allow creation of Pokemon objects
+    ##It then initiates the interface by calling main_menu() 
     def self.welcome()
-        #creation of Pokemon index
         print FX[:window_size]
         PokedexGo::Scraper.create_pokemon_from_index()
         main_menu()
     end
 
+    #Main Menu displays an intro graphic as well as four navigational options
     def self.main_menu()
-        #Root of menu system
         system("clear")
         window_title_single("Pokedex Go")
         puts "│                                   ,'\\                               │"
@@ -83,6 +86,10 @@ class PokedexGo::CLI
         end
     end
 
+    #Present Pokemon List takes in a list of pokemon objects and creates 
+    ##a paged interface consisting of 15 pokemon per page
+    ##It also gives the option to type in one of the pokemon's names
+    ##to see a detailed profile page, or to retreat to the main menu
     def self.present_pokemon_list(list)
         offset = 0
 
@@ -135,8 +142,9 @@ class PokedexGo::CLI
         end
     end
 
+    #List Pokemon of Type presents the user with a display containing all the possible pokemon types
+    #and allows the user to search for one, calling "present_pokemon_list()" with the pokemon of that type
     def self.list_pokemon_of_type()
-        #lists all pokemon of given type
         system("clear")
         window_title_single("Pokemon Types")
         puts "│                                                                     │"
@@ -166,6 +174,8 @@ class PokedexGo::CLI
         present_pokemon_list(pokemon_of_type)
     end
 
+    #Search by Name allows a user to search for a Pokemon by name. If more than one Pokemon
+    ##is found with the entered string, it defers to "select_from_pokemon()" before "view_pokemon_profile()"
     def self.search_by_name(user_input)
         pokemon_of_name = PokedexGo::Pokemon.all.select do |pokemon|
             pokemon.name.downcase.include?(user_input.downcase)
@@ -180,77 +190,8 @@ class PokedexGo::CLI
         end
     end
 
-    def self.view_pokemon_pve_movesets(pokemon)
-
-        system("clear")
-        window_title_single("PVE Movesets")
-        puts "│        Quick                    Charge          Atk Grade Def Grade │"
-        puts "├──────────────────────┬────────────────────────┬──────────┬──────────┤"
-        pokemon.pve_movesets.each_with_index do |moveset, index|
-            print "│"
-            print center_string(moveset[:quick], 22, " ")
-            print "│"
-            print center_string(moveset[:charge], 24, " ")
-            print "│"
-            print center_string(moveset[:atk_grade], 10, " ")
-            print "│"
-            print center_string(moveset[:def_grade], 10, " ")   
-            puts "│"
-            if((index + 1) < pokemon.pve_movesets.size)
-                puts "├──────────────────────┼────────────────────────┼──────────┼──────────┤"
-            else
-                puts "╰──────────────────────┴────────────────────────┴──────────┴──────────╯"
-            end
-        end
-
-        window_navigation_single("Type '1' to return to #{pokemon.name}'s profile")
-
-        user_input = 0
-
-        while user_input != 1
-            print "Return?: "
-            user_input = gets.chomp.to_i
-            if user_input == 1
-                view_pokemon_profile(pokemon)
-            end
-        end
-    end
-
-    def self.view_pokemon_pvp_movesets(pokemon)
-        system("clear")
-        window_title_single("PVP Movesets")
-        puts "│        Quick              Charge 1             Charge 2       Grade │"
-        puts "├────────────────────┬────────────────────┬────────────────────┬──────┤"
-        pokemon.pvp_movesets.each_with_index do |moveset, index|
-            print "│"
-            print center_string(moveset[:quick], 20, " ")
-            print "│"
-            print center_string(moveset[:charge_1], 20, " ")
-            print "│"
-            print center_string(moveset[:charge_2], 20, " ")
-            print "│"
-            print center_string(moveset[:grade], 6, " ")   
-            puts "│"
-            if((index + 1) < pokemon.pvp_movesets.size)
-                puts "├────────────────────┼────────────────────┼────────────────────┼──────┤"
-            else
-                puts "╰────────────────────┴────────────────────┴────────────────────┴──────╯"
-            end
-        end
-
-        window_navigation_single("Type '1' to return to #{pokemon.name}'s profile")
-
-        user_input = 0
-
-        while user_input != 1
-            print "Return?: "
-            user_input = gets.chomp.to_i
-            if user_input == 1
-                view_pokemon_profile(pokemon)
-            end
-        end
-    end
-
+    #Select from pokemon is called whenever a name search is done and the number of pokemon returned is
+    ##greater than 1. It allows the user to select from a list the specific pokemon they want.
     def self.select_from_pokemon(list)
         system("clear")
         offset = 0
@@ -278,6 +219,8 @@ class PokedexGo::CLI
         end
     end
 
+    #View Pokemon Profile presents the detailed profile of a selected pokemon and includes the options
+    ##to view PVP and PVE movesets, search for another pokemon, or return to the main menu
     def self.view_pokemon_profile(pokemon)
         #view full profile of individual pokemon
         PokedexGo::Scraper.add_profile_stats(pokemon)
@@ -374,12 +317,89 @@ class PokedexGo::CLI
 
     end
 
+    #View Pokemon PVE Movesets displays the PVE movesets of a specific pokemon in a list view
+    def self.view_pokemon_pve_movesets(pokemon)
+
+        system("clear")
+        window_title_single("PVE Movesets")
+        puts "│        Quick                    Charge          Atk Grade Def Grade │"
+        puts "├──────────────────────┬────────────────────────┬──────────┬──────────┤"
+        pokemon.pve_movesets.each_with_index do |moveset, index|
+            print "│"
+            print center_string(moveset[:quick], 22, " ")
+            print "│"
+            print center_string(moveset[:charge], 24, " ")
+            print "│"
+            print center_string(moveset[:atk_grade], 10, " ")
+            print "│"
+            print center_string(moveset[:def_grade], 10, " ")   
+            puts "│"
+            if((index + 1) < pokemon.pve_movesets.size)
+                puts "├──────────────────────┼────────────────────────┼──────────┼──────────┤"
+            else
+                puts "╰──────────────────────┴────────────────────────┴──────────┴──────────╯"
+            end
+        end
+
+        window_navigation_single("Type '1' to return to #{pokemon.name}'s profile")
+
+        user_input = 0
+
+        while user_input != 1
+            print "Return?: "
+            user_input = gets.chomp.to_i
+            if user_input == 1
+                view_pokemon_profile(pokemon)
+            end
+        end
+    end
+
+    #View Pokemon PVP Movesets displays the PVP movesets of a specific pokemon in a list view
+    def self.view_pokemon_pvp_movesets(pokemon)
+        system("clear")
+        window_title_single("PVP Movesets")
+        puts "│        Quick              Charge 1             Charge 2       Grade │"
+        puts "├────────────────────┬────────────────────┬────────────────────┬──────┤"
+        pokemon.pvp_movesets.each_with_index do |moveset, index|
+            print "│"
+            print center_string(moveset[:quick], 20, " ")
+            print "│"
+            print center_string(moveset[:charge_1], 20, " ")
+            print "│"
+            print center_string(moveset[:charge_2], 20, " ")
+            print "│"
+            print center_string(moveset[:grade], 6, " ")   
+            puts "│"
+            if((index + 1) < pokemon.pvp_movesets.size)
+                puts "├────────────────────┼────────────────────┼────────────────────┼──────┤"
+            else
+                puts "╰────────────────────┴────────────────────┴────────────────────┴──────╯"
+            end
+        end
+
+        window_navigation_single("Type '1' to return to #{pokemon.name}'s profile")
+
+        user_input = 0
+
+        while user_input != 1
+            print "Return?: "
+            user_input = gets.chomp.to_i
+            if user_input == 1
+                view_pokemon_profile(pokemon)
+            end
+        end
+    end
+
+    #quit makes sure the application exits successfully
     def self.quit()
         system("clear")
         window_banner("Goodbye!")
         exit(true)
     end
 
+    #The methods beginning with "window" are helper methods for constructing pieces of the text
+    ##based interface
+    ##Window Banner creates a large banner with a passed string in all caps, centered within it
     def self.window_banner(name)
         puts "╔═════════════════════════════════════════════════════════════════════╗"
         print "║"
@@ -388,6 +408,8 @@ class PokedexGo::CLI
         puts "╚═════════════════════════════════════════════════════════════════════╝"
     end
 
+    #Window Title Single creates a title bar for a window that stretches across the entire width
+    ##of the interface and includes the passed string, centered.
     def self.window_title_single(title)
         puts "╭─────────────────────────────────────────────────────────────────────╮"
         print "│"
@@ -398,6 +420,8 @@ class PokedexGo::CLI
         puts "├─────────────────────────────────────────────────────────────────────┤"
     end
 
+    #Window Title Double creates a title bar that is divided in the center to create two window title bars
+    ##It centers the passed strings within their respective title bar
     def self.window_title_double(title_1, title_2)
         puts "╭──────────────────────────────────┬──────────────────────────────────╮"
         print "│"
@@ -408,14 +432,18 @@ class PokedexGo::CLI
         puts "├──────────────────────────────────┼──────────────────────────────────┤"
     end
 
+    #Window Tail Single creates the bottom edge of a window that spans the entire width of the interface
     def self.window_tail_single()
         puts "╰─────────────────────────────────────────────────────────────────────╯"
     end
 
+    #Window Tail Double creates the bottom edge of a window that contains two sub-divisions
     def self.window_tail_double()
         puts "╰──────────────────────────────────┴──────────────────────────────────╯"
     end
 
+    #Window Navigation Single creates a single "bubble style" prompt with centered text
+    ##for use before receiving user input
     def self.window_navigation_single(prompt)
         puts "╭─────────────────────────────────────────────────────────────────────╮"
         print "│"
@@ -424,6 +452,8 @@ class PokedexGo::CLI
         puts "╰─────────────────────────────────────────────────────────────────────╯"
     end
 
+    #Window Navigation Quad creates a larger "Bubble Style" prompt with 4 sub-divisions for text
+    ##This allows more options to be presented to the user
     def self.window_navigation_quad(option_1, option_2, option_3, option_4)
         puts "╭──────────────────────────────────┬──────────────────────────────────╮"
         print "│"
@@ -440,6 +470,9 @@ class PokedexGo::CLI
         puts "╰──────────────────────────────────┴──────────────────────────────────╯"
     end
 
+    #Center String and Align String help build the titles and columns of the interface
+    ##by padding the left and right or just the right side of the string with a given
+    ##filler character
     def self.center_string(string, gap, filler_character)
         centered_string = ""
         ((gap - string.length) / 2).times {centered_string << filler_character}
